eb6f97de1b9b07b63c81f15ed86121638d73ea70
Sean Pivin <seanpivin@gmail.com>
2023-08-21 17:26:03 +0300

    store function been added

--- src/store.js
+++ src/store.js
@@ -39,14 +39,28 @@ function reducer(state = intialState, action) {
 }
 
 const store = createStore(reducer);
-store.dispatch({ type: "account/deposit", payload: 1000 });
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function collectInterest(amount, purpose) {
+  return { type: "account/collectInterest", payload: { amount, purpose } };
+}
+
+function payLoan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(1000));
 console.log(store.getState());
-store.dispatch({ type: "account/withdraw", payload: 100 });
+store.dispatch(withdraw(100));
 console.log(store.getState());
-store.dispatch({
-  type: "account/collectInterest",
-  payload: { amount: 800, purpose: "Buy an xbox" },
-});
+store.dispatch(collectInterest(950, "Buy an iphone"));
 console.log(store.getState());
-store.dispatch({ type: "account/payLoan" });
+store.dispatch(payLoan());
 console.log(store.getState());
