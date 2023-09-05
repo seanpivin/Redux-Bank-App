9d4caa9b577750bb552b2a2ff144486ba408a74b
Sean Pivin <seanpivin@gmail.com>
2023-09-05 10:45:59 +0300

    removing all un N code

--- src/features/customer/customerSlice.js
+++ src/features/customer/customerSlice.js
@@ -29,33 +29,3 @@ const customerSlice = createSlice({
 export const { createCustomer, updateName } = customerSlice.actions;
 
 export default customerSlice.reducer;
-
-/*export default function reducerCustomer(state = intialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        name: action.payload.name,
-        id: action.payload.id,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateName":
-      return {
-        ...state,
-        name: action.payload,
-      };
-    default:
-      return state;
-  }
-}
-
-export function createCustomer(name, id, createdAt) {
-  return {
-    type: "customer/createCustomer",
-    payload: { name, id, createdAt: new Date().toISOString() },
-  };
-}
-
-export function updateName(name) {
-  return { type: "customer/updateName", payload: name };
-}*/
